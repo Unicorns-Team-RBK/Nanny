3071902648c0395bb3a13a23c1cc45bef8e11157
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-01-03 19:26:53 +0100

    setup ready

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+      <div className="Navbar">
+      <h1>hello world</h1>
+      </div>
+    );
+  }
+  
+  export default Navbar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Navbar from './Components/Navbar'
 
 ReactDOM.render(
-  <App />,
+  <Navbar />,
   document.getElementById('root')
 );
 
