63986a461d171701bba2c658aa3ba3b74c98ee61
MeriemBouguila <marialanavetti@gmail.com>
2021-01-05 17:15:34 +0100

    nanyform_nanydata

---
+++ frontend/src/Components/ForNannies/NanniesForm.jsx
@@ -0,0 +1,63 @@
+//here is the form 'NanniesForm' that a nanny will fill when signing up. these inputs require her first name ,
+//her last name ,her email address ,her phone number and her password .Then she can add some informations
+//about herself for example her experiences , what she likes what she's good at why a parent will prefer her 
+//over other nanies .
+
+
+var NanniesForm = function () {
+    return (
+        <form>
+            <div class="wrap">
+
+                <div class="row">
+                    <div class="columns text-center">
+                        <h5>Nanny's Information</h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="columns special">
+                        <input class="required" placeholder="First Name*" name="first_name" type="text"></input>
+                    </div>
+                    <div class="columns">
+                        <input class="required" placeholder="Last Name*" name="last_name" type="text"></input>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="columns">
+                        <input id="email" class="required email" placeholder="E-mail Address*" name="email_address" type="email"></input>
+                    </div>
+                    <div class="columns">
+                        <input id="phone" class="required" placeholder="Phone Number*" name="phone" type="text"></input>
+                    </div>
+                </div>
+            </div>
+
+            <div class="wrap">
+                <div class="row">
+                    <div class="columns text-center">
+                        <h5>Write your password</h5>
+                    </div>
+                    <div class="columns">
+                        <input id="password" class="required" placeholder="your password*" name="password" type="password"></input>
+                    </div>
+                </div>
+            </div>
+
+            <div class="wrap">
+                <div class="row">
+                    <div class="columns text-center">
+                        <h5>Tell us something about yourself</h5>
+
+                    </div>
+                    <div class="columns">
+                        <textarea id="about_nanny" class="not required" placeholder="what are you good at ?why a parent will prefer you over other nannies ?" rows="10" cols="150" name="aboutnany"></textarea>
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default NanniesForm;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,10 @@ import './index.css';
 import Homepage from './Components/HomePage'
 // import ForParent from './Components/ForParents/ForParents'
 // import ForNannies from './Components/ForNannies/ForNannies'
+//import NanniesForm from './Components/ForNannies/NanniesForm'
+//import NanniesAccount from './Components/ForNannies/NanniesAccount'
+//import ParentsForm from './Components/ForParents/ParentsForm'
+//import ParentsAccount from './Components/ForParents/ParentsAccount'
 
 ReactDOM.render(
   <Homepage />,
