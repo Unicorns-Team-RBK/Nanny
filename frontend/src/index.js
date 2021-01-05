1c69b07e0a757a7b0f752efd9e5a617e10267b02
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-01-05 11:18:08 +0100

    adding css files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //IMPORTING ALL THE COMPONENTS 
-// import Navbar1 from '../src/Components/Navbar1'
+import Navbar1 from '../src/Components/Navbar1'
 // import Navbar2 from '../src/Components/Navbar2'
-
 // import Homepage from './Components/HomePage'
 // import ForParent from './Components/ForParents/ForParents'
-import ForNannies from './Components/ForNannies/ForNannies'
+// import ForNannies from './Components/ForNannies/ForNannies'
 
 ReactDOM.render(
-  <ForNannies />,
+  <Navbar1 />,
   document.getElementById('root')
 );
 
