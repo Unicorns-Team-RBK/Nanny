725a34c84a8fe58dbe5c3a704d4314e25707fd31
MeriemBouguila <marialanavetti@gmail.com>
2021-01-05 22:06:15 +0100

    change to className

--- frontend/src/Components/ForParents/ParentsForm.jsx
+++ frontend/src/Components/ForParents/ParentsForm.jsx
@@ -1,85 +1,85 @@
 var ParentForm = function () {
     return (
         <form>
-            <div class="wrap">
+            <div className="wrap">
 
-                <div class="row">
-                    <div class="columns text-center">
+                <div className="row">
+                    <div className="columns text-center">
                         <h5>Parent Information</h5>
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="columns special">
-                        <input class="required" placeholder="First Name*" name="first_name" type="text"></input>
+                <div className="row">
+                    <div className="columns special">
+                        <input className="required" placeholder="First Name*" name="first_name" type="text"></input>
                     </div>
-                    <div class="columns">
-                        <input class="required" placeholder="Last Name*" name="last_name" type="text"></input>
+                    <div className="columns">
+                        <input className="required" placeholder="Last Name*" name="last_name" type="text"></input>
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="columns">
-                        <input id="email" class="required email" placeholder="E-mail Address*" name="email_address" type="email"></input>
+                <div className="row">
+                    <div className="columns">
+                        <input id="email" className="required email" placeholder="E-mail Address*" name="email_address" type="email"></input>
                     </div>
-                    <div class="columns">
-                        <input id="phone" class="required" placeholder="Phone Number*" name="phone" type="text"></input>
+                    <div className="columns">
+                        <input id="phone" className="required" placeholder="Phone Number*" name="phone" type="text"></input>
                     </div>
                 </div>
             </div>
 
-            <div class="wrap">
-                <div class="row">
-                    <div class="columns text-center">
+            <div className="wrap">
+                <div className="row">
+                    <div className="columns text-center">
                         <h5>Write your password</h5>
                     </div>
-                    <div class="columns">
-                        <input id="password" class="required" placeholder="your password*" name="password" type="password"></input>
+                    <div className="columns">
+                        <input id="password" className="required" placeholder="your password*" name="password" type="password"></input>
                     </div>
                 </div>
             </div>
 
-            <div class="wrap">
-                <div class="row">
-                    <div class="columns text-center">
+            <div className="wrap">
+                <div className="row">
+                    <div className="columns text-center">
                         <h5>Notes</h5>
 
                     </div>
-                    <div class="columns">
-                        <textarea id="about_children" class="required" placeholder="want to precise something? What type of childcare are you looking for ?" rows="5" cols="150" name="aboutchild"></textarea>
+                    <div className="columns">
+                        <textarea id="about_children" className="required" placeholder="want to precise something? What type of childcare are you looking for ?" rows="5" cols="150" name="aboutchild"></textarea>
                     </div>
                 </div>
             </div>
 
-            <div class="wrap">
+            <div className="wrap">
 
-                <div class="row">
-                    <div class="columns text-center">
+                <div className="row">
+                    <div className="columns text-center">
                         <h5>Home Address</h5>
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="columns">
-                        <input id="street" class="required" placeholder="Street*" name="street" type="text"></input>
+                <div className="row">
+                    <div className="columns">
+                        <input id="street" className="required" placeholder="Street*" name="street" type="text"></input>
                     </div>
-                    <div class="columns">
+                    <div className="columns">
                         <input id="apartment" placeholder="Apartment Number" name="apartment" type="text"></input>
                     </div>
 
 
-                    <div class="columns">
-                        <input id="city" class="required" placeholder="City*" name="city" type="text"></input>
+                    <div className="columns">
+                        <input id="city" className="required" placeholder="City*" name="city" type="text"></input>
 
                     </div>
 
-                    <div class="columns">
-                        <input class="required" placeholder="Zip Code*" type="text" name="zip"></input>
+                    <div className="columns">
+                        <input className="required" placeholder="Zip Code*" type="text" name="zip"></input>
                     </div>
                 </div>
-                <div class="row">
-				<div class="columns text-center">
-					<input class="btn submit" type="submit" value="Submit Application"></input>
+                <div className="row">
+				<div className="columns text-center">
+					<input className="btn submit" type="submit" value="Submit Application"></input>
 				</div>
 			</div>
             </div>
