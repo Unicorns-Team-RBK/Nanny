bdbdb8f935ae074929e13eaded3d1d165979e21b
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-01-05 19:38:08 +0100

    changing class to className

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //IMPORTING ALL THE COMPONENTS 
-import Navbar1 from '../src/Components/Navbar1'
+// import Navbar1 from '../src/Components/Navbar1'
 // import Navbar2 from '../src/Components/Navbar2'
 // import Homepage from './Components/HomePage'
 // import ForParent from './Components/ForParents/ForParents'
 // import ForNannies from './Components/ForNannies/ForNannies'
-//import NanniesForm from './Components/ForNannies/NanniesForm'
+import NanniesForm from './Components/ForNannies/NanniesForm'
 //import NanniesAccount from './Components/ForNannies/NanniesAccount'
 //import ParentsForm from './Components/ForParents/ParentsForm'
 //import ParentsAccount from './Components/ForParents/ParentsAccount'
 
 ReactDOM.render(
-  <Navbar1 />,
+  <NanniesForm />,
   document.getElementById('root')
 );
 
