e3c1dbbe39ed5869ef0939283f79573a4157119d
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-01-06 13:37:44 +0100

    done render just prob w/ login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
 import React from "react";
-// import React, { useState, useEffect } from "react";
-// import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 // import Axios from "axios";
 
 //IMPORTING ALL THE COMPONENTS 
-// import Header  from './Components/Header'
-// import Homepage from './Components/HomePage'
-// import Login from './Components/Login'
+import Header  from './Components/Header'
+import Homepage from './Components/HomePage'
+import Login from './Components/Login'
 
-// import ForParent from './Components/ForParents/ForParents'
-// import ForNannies from './Components/ForNannies/ForNannies'
-// import ParentsForm from './Components/ForParents/ParentsForm'
-// import NanniesForm from './Components/ForNannies/NanniesForm'
-//import NanniesAccount from './Components/ForNannies/NanniesAccount'
-//import ParentsAccount from './Components/ForParents/ParentsAccount'
+import ForParent from './Components/ForParents/ForParents'
+import ForNannies from './Components/ForNannies/ForNannies'
+import ParentsForm from './Components/ForParents/ParentsForm'
+import NanniesForm from './Components/ForNannies/NanniesForm'
+// import NanniesAccount from './Components/ForNannies/NanniesAccount'
+// import ParentsAccount from './Components/ForParents/ParentsAccount'
 
 export default function App() {
-
-    
-    // return < />
+return (
+    <>
+    <BrowserRouter>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/ForParent" component={ForParent} />
+            <Route path="/ForNannies" component={ForNannies} />
+            <Route path="/ParentsForm" component={ParentsForm} />
+            <Route path="/NanniesForm" component={NanniesForm} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+  
+    </BrowserRouter>
+  </>
+);
 }
